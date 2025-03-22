a894c22b6d8fd8c3e88ade5bb543d3fa29fa4450
im-satyam <157481814+im-satyam@users.noreply.github.com>
2025-03-23 04:14:38 +0530

    Getting difff functions

--- server/router/issueRouter.js
+++ server/router/issueRouter.js
@@ -7,6 +7,9 @@ const {
   issueActive,
   getIssuesById,
   updateStat,
+  getActiveIssue,
+  getCompletedIssue,
+  getUnderIssue,
 } = require("../controllers/issueController");
 const { updateUser } = require("../controllers/authController");
 
@@ -17,4 +20,7 @@ router.get("/check-user-issues", authMiddleware, getUserIssue);
 router.post("/get-id-issues", authMiddleware, getIssuesById);
 router.put("/change-status", authMiddleware, issueActive);
 router.put("/users/:id", updateStat);
+router.get("/active-issues", authMiddleware, getActiveIssue);
+router.get("/completed-issues", authMiddleware, getCompletedIssue);
+router.get("/under-issues", authMiddleware, getUnderIssue);
 module.exports = router;
